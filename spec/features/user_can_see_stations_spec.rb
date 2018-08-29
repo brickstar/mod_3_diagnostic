cef4cb9c33815c64fe903ff00bfbd430e826cffd
brickstar <brickstar@gmail.com>
2018-08-29 10:58:47 -0600

    remove user story from test

--- spec/features/user_can_see_stations_spec.rb
+++ spec/features/user_can_see_stations_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'user visits /' do
   describe 'can click link locate and land on /search' do
-    it 'sees stations' do
+    it 'sees 10 stations within 6 miles of 80203' do
       visit "/"
 
       fill_in "q", with: "80203"
@@ -26,14 +26,3 @@ describe 'user visits /' do
     end
   end
 end
-
-=begin
-As a user
-When I visit "/"
-And I fill in the search form with 80203 (Note: Use the existing search form)
-And I click "Locate"
-Then I should be on page "/search"
-Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-And the stations should be limited to Electric and Propane
-And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
-=end
