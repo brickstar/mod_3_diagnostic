ccd7a11f7c263d38cf73b06977bd1d425c7cf07d
brickstar <brickstar@gmail.com>
2018-08-29 09:40:55 -0600

    getting data, next step is creating station class

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def index
+    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?location=80203?fuel_type=LPG,ELEC&radius=6&limit=10&api_key=UFGTdkhcnf13xrn7OfWD6SxHAe9COpOICg9CIAUu&format=JSON")
 
+    raw_stations = JSON.parse(response.body, symbolize_names: true)
+
+    @stations = raw_stations.map do |raw_station|
+      Station.new(raw_station)
+    end
   end
 end
