dac99b20529dca8f90f9ed14d81659b6c56f54d5
brickstar <brickstar@gmail.com>
2018-08-29 09:25:06 -0600

    feature spec and some stuff in view

---
+++ spec/features/user_can_see_stations_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'user visits /' do
+  describe 'can click link locate and land on /search' do
+    it 'sees stations' do
+      visit "/"
+
+      fill_in "q", with: "80203"
+
+      click_on "Locate"
+
+      expect(current_path).to eq("/search")
+
+      within(first(".station")) do
+        expect(page).to have_css(".name")
+        expect(page).to have_css(".address")
+        expect(page).to have_css(".fuel_types")
+        expect(page).to have_css(".distance")
+        expect(page).to have_css(".access_times")
+      end
+    end
+  end
+end
+
+=begin
+As a user
+When I visit "/"
+And I fill in the search form with 80203 (Note: Use the existing search form)
+And I click "Locate"
+Then I should be on page "/search"
+Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+And the stations should be limited to Electric and Propane
+And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+=end
